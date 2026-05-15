96f96d4518625eae6b419464953c0073fd2583cc
Henry Chen <chenliuheng@163.com>
2026-05-15 23:05:27 +0800

    auto backup

--- get_hn.ps1
+++ get_hn.ps1
@@ -1,11 +1,11 @@
-$ids = @(47679121,47676509,47679258,47680404,47677853,47650887,47678573,47681566,47657268,47680309,47681112,47673360,47654626,47681361,47677885,47675625,47658104,47675893,47675213,47681274,47664836,47673005,47621133,47627217,47682557,47675302,47677241,47679155)
+$ids = @(48142251,48138136,48140730,48142108,48139219,48142193,48137145,48138268,48140529,48140953,48141191,48140541,48097786,48141732,48137553,48140922,48108621,48139316,48134743,48136262)
 $results = @()
 foreach ($id in $ids) {
     try {
-        $r = Invoke-RestMethod -Uri "https://hacker-news.firebaseio.com/v0/item/$id.json" -TimeoutSec 5
-        if ($r -and $r.title) {
-            $results += "$($r.title)"
+        $item = Invoke-RestMethod -Uri "https://hacker-news.firebaseio.com/v0/item/$id.json" -TimeoutSec 10 -ErrorAction SilentlyContinue
+        if ($item -and $item.title) {
+            $results += $item
         }
     } catch {}
 }
-$results | Select-Object -First 20 | ForEach-Object { Write-Output $_ }
+$results | Select-Object title, url, score | ConvertTo-Json -Depth 1
